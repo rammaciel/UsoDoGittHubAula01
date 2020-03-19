12a61ff4eea200c2c49b027d42642463e392d630
unknown <robertoaugusto00@outlook.com>
2020-03-19 19:42:14 -0300

    Versao 4

--- src/aula00/Aluno.java
+++ src/aula00/Aluno.java
@@ -1,14 +1,24 @@
 package aula00;
 
 public class Aluno{
-   private String nome;
-   private int id;
-public int getId() {
-	return id;
-}
-public void setId(int id) {
-	this.id = id;
-}
+	private String nome;
+	private int id;
+	  
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
    
 }
    
